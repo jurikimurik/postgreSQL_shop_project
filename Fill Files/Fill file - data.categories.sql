3d251eac195d187dcc1be1e97f3b76d69c31c79a
JanWojtowski <120735941+JanWojtowski@users.noreply.github.com>
2023-06-11 19:41:27 +0200

    Add fill files

---
+++ Fill Files/Fill file - data.categories.sql
@@ -0,0 +1,8 @@
+insert into data.categories (category) values ('Tops');
+insert into data.categories (category) values ('Bottoms');
+insert into data.categories (category) values ('One piece');
+insert into data.categories (category) values ('Outerwear');
+insert into data.categories (category) values ('Intimates');
+insert into data.categories (category) values ('Footwear');
+insert into data.categories (category) values ('Accessories');
+insert into data.categories (category) values ('Other');
---
+++ Fill Files/Fill file - data.payments.sql
@@ -0,0 +1,3 @@
+insert into data.payments (payment_method) values ('Cash');
+insert into data.payments (payment_method) values ('Card');
+
